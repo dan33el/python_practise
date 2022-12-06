c3a93e02e2a0e52cbfd20bf3c2abe0232b373270
dan33el <dadmihil@gmail.com>
2022-12-06 16:02:30 +0800

    topik_lists_finished

---
+++ lists/find_second_index.py
@@ -0,0 +1,16 @@
+def find_index(value, items):
+    for index, item in enumerate(items):
+        if item == value:
+            return index
+
+print(find_index('a', 'baobao'))
+
+def find_second_index(value, items):
+    cursor = iter(items)
+    first_index = find_index(value, cursor)
+    second_index = find_index(value, cursor)
+    if second_index is not None:
+        return first_index + second_index + 1
+        
+
+print(find_second_index('a', 'baabo'))
